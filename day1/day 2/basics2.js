83a726fa575de52f8ff0f5ca0021012ba2ec5fc4
iamajaypandit <ap971092@gmail.com>
2026-07-21 23:59:18 +0530

    js practice on loops

---
+++ day1/day 2/basics2.js
@@ -0,0 +1,82 @@
+//Multiplication Table Generator for a Given Number
+//Write a program that takes a variable named input and assign 
+//its value as 8 and prints its multiplication table upto 10.
+// let n=8;
+// for(let i =1; i<=10; i++){
+// if(i*n){
+//     console.log(`${i}x${n}=${i*n}`);
+// }
+// }
+//Sum of Even Numbers
+//You are given a number N. Print sum of all even numbers from 1 to N.
+// let n = 50;
+// let sum =0;
+// for(let i=1; i<=n; i++){
+//     if(i%2==0){
+//         sum+=i;
+//         console.log(sum);
+//     }
+// }
+//Factorial with loop
+// Note: Factorial of any given number n is represented as n! = n * n-1 * n-2 .....* 1.
+// let n=6;
+// let fact=1;
+// for(let i=1; i<=n; i++){
+//       fact=fact*i;
+//       console.log(fact);
+
+// }
+//Check Prime
+// Write a program that inputs a positive integer N. 
+// It should then output a message indicating whether the number is a prime number or not.
+// let n= 50;
+// let isPrime=false;
+// if(n<=1){
+//     isPrime=false;
+// }else{
+//     for(let i=2; i<n; i++){
+//         if(n%i===0){
+//             isPrime=false;
+//             break;
+//         }
+//     }
+// }
+// if(isPrime){
+//     console.log(n+"is a  Prime number");
+// }
+// else{
+//     console.log(n+"is not a prime number");
+// }
+
+//Digit Frequency Counter
+//Given a number N and a digit D. Write a program to find how many times the digit D appears in the number N.
+// let n = 151111524;
+// let d = 1;
+// let count = 0;
+// while (n > 0) {
+//     let last_digit = n % 10;
+
+//     if (last_digit == d) {
+//         count++;
+//     }
+//     n = Math.floor(n / 10);
+// }
+// console.log(count);
+//Palindrome Number Checker
+// Given a number N, you need to check 
+// whether the given number is Palindrome or not. 
+// A number is said to be Palindrome when it reads the same from backward as forward.
+function checkPalindrome(str){
+let rev="";
+for(let i=str.length-1;  i>=0; i--){
+    rev=rev+str[i];
+}
+if(str === rev){
+    console.log("Palindrome");
+}else{
+    console.log("Not Palindrome");
+}
+}
+checkPalindrome("madam");
+checkPalindrome("hello");
+checkPalindrome("rabina");
