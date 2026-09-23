848d11100a83d4029fc821b3bd8fd7bf8206985d
iamajaypandit <ap971092@gmail.com>
2026-09-24 00:53:04 +0530

    matrix zig zig added

--- day1/matrix.js
+++ day1/matrix.js
@@ -1,11 +1,11 @@
 // printing daigonal matrixs
 
-let mat = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-];
+// let mat = [
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
+// ];
 
 // for(let row=mat.length-1;row>=0;row--){
 //     for(let col=0;col<mat[row].length;col++){
@@ -37,13 +37,46 @@ let mat = [
 // }
 // 1 6 11 16 
 
-for(let j=mat[0].length-1; j>=0; j--){
-    for(let i=0; i<mat.length; i++){
-        if(i==j){
-            console.log(mat[i][j]);
-        }
-    }
-}
+// for(let j=mat[0].length-1; j>=0; j--){
+//     for(let i=0; i<mat.length; i++){
+//         if(i==j){
+//             console.log(mat[i][j]);
+//         }
+//     }
+// }
 // 16 11 6 1
 
+let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [8,9,10]
+];
+let result=[];
+for(let i=0; i<arr[0].length; i++){
+    for(let j=0; j<arr.length; j++){
+        result.push(arr[j][i]);
+    }
+}
+console.log(result);
+//[1,4,8,2,5,9,3,6,10]
+
+let mat = [
+    [1, 2, 3, 4],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11]
+]
+
+    for (let i = 0; i < mat.length; i++) {
+        let result = "";
+        if (i % 2 == 0) {
+            for (let j = 0; j < mat[0].length; j++) {
+                result += mat[i][j] + " ";
+            }
+        } else {
+            for (let j = mat[0].length - 1; j >= 0; j--) {
+                result += mat[i][j] + " ";
+            }
+        }
+        console.log(result);
+    }
 
